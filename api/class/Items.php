3d894b4907e49446e96e96ce406ae7cfd41da21e
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2021-05-19 11:14:39 +0200

    Update Items.php

--- api/class/Items.php
+++ api/class/Items.php
@@ -67,3 +67,22 @@ class Items{
 	 
 		return false;
 	}
+
+    function delete(){
+		
+		$stmt = $this->conn->prepare("
+			DELETE FROM ".$this->itemsTable." 
+			WHERE id = ?");
+			
+		$this->id = htmlspecialchars(strip_tags($this->id));
+	 
+		$stmt->bind_param("i", $this->id);
+	 
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;		 
+	}
+}
+?>
