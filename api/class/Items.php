fc9e2de011d5f5e9e02e7bbf3a8e2ad79f41a0ca
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2021-05-19 11:12:24 +0200

    Update Items.php

--- api/class/Items.php
+++ api/class/Items.php
@@ -12,3 +12,16 @@ class Items{
     public function __construct($db){
         $this->conn = $db;
     }	
+
+    function read(){	
+		if($this->id) {
+			$stmt = $this->conn->prepare("SELECT * FROM ".$this->itemsTable." WHERE id = ?");
+			$stmt->bind_param("i", $this->id);					
+		} else {
+			$stmt = $this->conn->prepare("SELECT * FROM ".$this->itemsTable);		
+		}		
+		$stmt->execute();			
+		$result = $stmt->get_result();		
+		return $result;	
+	}
+	
