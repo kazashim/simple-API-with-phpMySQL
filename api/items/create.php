173321b791eabeb2e81686a535ba17a17ace8577
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2021-05-19 11:10:45 +0200

    Update create.php

--- api/items/create.php
+++ api/items/create.php
@@ -16,3 +16,8 @@ $items = new Items($db);
 $data = json_decode(file_get_contents("php://input"));
 
 if(!empty($data->email) && !empty($data->industry) && !empty($data->country) && !empty($data->created_date)){    
+    
+    $items->email = $data->email;
+    $items->industry = $data->industry;
+    $items->country = $data->country;
+    $items->created_date = date('Y-m-d H:i:s');
